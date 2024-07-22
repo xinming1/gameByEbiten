3fbd698d80366d7afb5dc4d62ca7d69951c422e5
zhihao.guo <zhihao.guo@plaso.me>
2024-07-22 17:53:03 +0800

    feat: 支持向右移动水平翻转

--- game/dog.go
+++ game/dog.go
@@ -39,36 +39,38 @@ func NewDog(dogConfig *config.DogConfig) *Dog {
 	dog.x = (float64(config.Cfg.ScreenWidth) - dog.width) / 2
 	dog.y = (float64(config.Cfg.ScreenHeight) - dog.height) / 2
 	dog.imgOptions = &ebiten.DrawImageOptions{}
-	//dog.imgOptions.GeoM.Translate(dog.x, dog.y)
+	dog.imgOptions.GeoM.Translate(dog.x, dog.y)
 	return dog
 }
 
 func (d *Dog) Draw(screen *ebiten.Image) {
 
-	op := d.imgOptions
-
+	op := &ebiten.DrawImageOptions{}
+	if d.direction != left {
+		op.GeoM.Scale(-1, 1)
+		d.lastDirection = d.direction
+	}
+	op.GeoM.Translate(d.x, d.y)
 	screen.DrawImage(d.image, op)
+
+	//d.imgOptions.GeoM.Translate(d.x, d.y)
+	//screen.DrawImage(d.image, d.imgOptions)
 }
 
 func (d *Dog) Update() {
 	switch {
 	case ebiten.IsKeyPressed(ebiten.KeyLeft):
 		d.x -= d.speed
-		if d.direction != left {
-			d.imgOptions.GeoM.Invert()
-		}
+		d.direction = left
 	case ebiten.IsKeyPressed(ebiten.KeyRight):
 		d.x += d.speed
 		d.direction = right
-		if d.direction != right {
-			d.imgOptions.GeoM.Invert()
-		}
 	case ebiten.IsKeyPressed(ebiten.KeyUp):
 		d.y -= d.speed
 	case ebiten.IsKeyPressed(ebiten.KeyDown):
 		d.y += d.speed
 
 	}
-	d.imgOptions.GeoM.Translate(d.x, d.y)
+	//d.imgOptions.GeoM.Translate(d.x, d.y)
 
 }
